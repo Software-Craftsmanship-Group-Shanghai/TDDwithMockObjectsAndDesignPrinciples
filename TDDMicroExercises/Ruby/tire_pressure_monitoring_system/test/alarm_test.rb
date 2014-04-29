57af06a67dc841436741cb0016db5841563123ab
josephyao <dermott@126.com>
2014-04-29 23:55:08 +0800

    update the first test

--- TDDMicroExercises/Ruby/tire_pressure_monitoring_system/test/alarm_test.rb
+++ TDDMicroExercises/Ruby/tire_pressure_monitoring_system/test/alarm_test.rb
@@ -2,8 +2,8 @@ require 'test/unit'
 require_relative '../alarm'
 
 class AlarmTest < Test::Unit::TestCase
-  def test_do_something
+  def test_alarm_is_off_after_it_initialized
     alarm = Alarm.new
-    alarm.check
+    assert_equal false, alarm.alarm_on
   end
 end
