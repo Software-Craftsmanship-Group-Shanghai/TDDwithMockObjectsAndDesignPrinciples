1b60e1758211c5628c6077e8de78d838bc8a79c8
Joy Ge <joy.ge@oocl.com>
2014-05-26 14:53:35 +0800

    Refactor existing test and add a new test

---
+++ TDDMicroExercises.YoursSolutions/Java-ChinaKataRelay-TicketDispenser/src/test/java/TicketDispenserTest.java
@@ -0,0 +1,45 @@
+import org.junit.Before;
+import org.junit.Test;
+import tddmicroexercises.turnticketdispenser.TicketDispenser;
+import tddmicroexercises.turnticketdispenser.TurnTicket;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class TicketDispenserTest {
+
+    private TicketDispenser ticketDispenser;
+    private TurnTicket dispensedTicket;
+
+    @Before
+    public void setUp() throws Exception {
+        ticketDispenser = new TicketDispenser();
+        dispensedTicket = ticketDispenser.getTurnTicket();
+    }
+
+    @Test
+    public void a_new_ticket_must_have_the_turn_number_larger_than_a_dispensed_ticket() {
+
+        TurnTicket newTicket = ticketDispenser.getTurnTicket();
+
+        assertTrue(newTicket.getTurnNumber() > dispensedTicket.getTurnNumber());
+    }
+
+    @Test
+    public void a_new_ticket_must_have_the_turn_number_larger_than_a_dispensed_ticket_from_another_dispenser() {
+
+        TurnTicket newTicket = getTurnTicketFromAnotherDispenser();
+
+        // Assert
+        assertTrue(newTicket.getTurnNumber() > dispensedTicket.getTurnNumber());
+
+
+    }
+
+    private TurnTicket getTurnTicketFromAnotherDispenser() {
+        TicketDispenser anotherDispenser = new TicketDispenser();
+        return anotherDispenser.getTurnTicket();
+    }
+
+    // TODO: after ticket 10 comes ticket 11
+
+}
